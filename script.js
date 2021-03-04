62042142e8ca8839ba61ac24a2a24ba350b10e2f
Riller <rvsriller@gmail.com>
2021-03-04 14:38:23 -0300

    Game over function defined

--- script.js
+++ script.js
@@ -52,12 +52,23 @@ function update (event){
 
 
 function startGame(){
+   
     /*The snake needs to continue moving when it get close to the border. 
     The opposite side is set in the code bellow  */
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+   
+   
+    //Game over function in case of snake touch the own body
+    for( i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert("Game Over :(");
+        }
+    }
+
 
     createBackground();
     createSnake();
